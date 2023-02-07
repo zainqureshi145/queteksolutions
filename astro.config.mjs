14014c3903b5176cf47b058301a9c58cd8cb87f8
Zain Qureshi <zain@Zains-MacBook-Air.local>
2023-02-08 04:58:37 +0500

    fixed all

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,5 +17,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.queteksolutions.com',
   integrations: [tailwind(), alpinejs(), image(), mdx(), sitemap()]
 });
