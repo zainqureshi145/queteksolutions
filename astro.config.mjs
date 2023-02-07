bfcd3488b5067916008679339204dddbc4c2fac9
Zain <zainqureshi145@gmail.com>
2023-02-08 03:20:29 +0500

    contact form updted, firefox, safari issues resolved

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,5 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
   integrations: [tailwind(), alpinejs(), image(), mdx(), sitemap()]
 });
